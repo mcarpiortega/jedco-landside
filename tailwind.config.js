5580ccfdee1947c097c86e15a64d92b21242e65b
mcarpiortega <mcarpiortega@gmail.com>
2026-05-18 09:28:56 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'base': '#05050a',
+        'surface': '#0f0f17',
+        'panel': '#161621',
+        'border': '#1e1e2e',
+      },
+      fontFamily: {
+        mono: ['JetBrains Mono', 'Fira Code', 'monospace'],
+      },
+    },
+  },
+  plugins: [],
+}
